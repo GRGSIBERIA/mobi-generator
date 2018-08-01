08073f5505853c884c0605c2515458d372dfdea0
grgsiberia <nanashi4129@gmail.com>
2018-08-01 19:28:35 +0900

    manifestのテスト

---
+++ test/mobi/opf/manifest-node-test.rb
@@ -0,0 +1,21 @@
+#-*- encoding: utf-8
+
+class ManifestNodeTest < Minitest::Test
+    include Mobi::OPF
+
+    def setup
+        @p = PackageNode.new
+        @items = [
+            ItemContainer.new("./index.html"),
+            ItemContainer.new("./test.jpg")
+        ]
+        @n = ManifestNode.new(@p, @items)
+    end
+    
+    def test_new
+        assert_equal @n.node.name, "manifest"
+        
+        assert_equal @n.node.children[0].name, "item"
+        assert_equal @n.node.children[0].attribute("id").to_s, "__index_html"
+    end
+end
