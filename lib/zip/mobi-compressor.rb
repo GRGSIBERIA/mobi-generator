39dcb3a168dbc1741866de3991c364d9829a004e
grgsiberia <nanashi4129@gmail.com>
2018-08-02 18:46:41 +0900

    ZIPファイルを圧縮・解凍する回り

---
+++ lib/zip/mobi-compressor.rb
@@ -0,0 +1,10 @@
+#-*- encoding: utf-8
+
+module Mobi
+    module Zip
+        # Mobiファイル自体を生成するためのクラス
+        class MobiCompressor
+
+        end
+    end
+end
---
+++ lib/zip/old/zip-extractor.rb
@@ -0,0 +1,28 @@
+#-*- encoding: utf-8
+require "zip"
+
+module Mobi
+    module Zip
+        module Old
+            #
+            # ZIPファイルを展開し，ZipFileの配列を作成する
+            #
+            class ZipExtractor
+                # @return [Array<ZipFile>] 展開されたZipFileの配列
+                attr_reader :files
+
+                # ZIPファイルを展開する
+                # @param [StringIO] input_file 入力するZIPファイル
+                # @param [IO] input_file 入力するZIPファイル
+                def initialize(input_file)
+                    Zip::InputStream.open(input_file) do |input|
+                        @files = []
+                        while (entry = input.get_next_entry)
+                            @files << ZipFile.new(entry.name, entry.get_input_stream.sysread)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
