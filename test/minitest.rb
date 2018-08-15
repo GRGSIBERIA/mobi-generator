43589f8c6fdd59b4ee9ff5fd7b07e06edf284d88
grgsiberia <nanashi4129@gmail.com>
2018-08-15 15:09:27 +0900

    NodeBaseを移動

--- test/minitest.rb
+++ test/minitest.rb
@@ -17,7 +17,7 @@ require "./lib/mobi/parser/mathml"
 
 require "./lib/mobi/opf/errors"
 require "./lib/mobi/opf/item-container"
-require "./lib/mobi/opf/node-base"
+require "./lib/mobi/node-base"
 
 require "./lib/mobi/zip/mobi-compressor"
 require "./lib/mobi/zip/zip-extractor"
@@ -36,10 +36,11 @@ require "./lib/mobi/opf/opf-generator"
 # テスト本体
 #
 require "./test/mobi/parser/ruby-parser-test"
-#require "./test/mobi/parser/mathml-parser-test" # texmathの実行ファイルにパスを通す
+require "./test/mobi/parser/mathml-parser-test" # texmathの実行ファイルにパスを通す
 
 
-require "./test/mobi/opf/node-base-test"
+require "./test/mobi/node-base-test"
+
 require "./test/mobi/opf/package-node-test"
 require "./test/mobi/opf/metadata-node-test"
 require "./test/mobi/opf/item-container-test"
